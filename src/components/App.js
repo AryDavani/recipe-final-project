b694e13baff6de11dba555e9579355add00f6733
Ary Davani <arydavani@gmail.com>
2017-10-03 10:31:07 -0400

    form added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  _handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.recipe.value);
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>hello</h1>
+      <div className="container">
+        <form onSubmit={ this._handleFormSubmit }>
+          <div className="form-group">
+            <p>Paste a recipe below</p>
+            <textarea name="recipe" className="form-control" rows="3"></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">Calculate Foods</button>
+        </form>
       </div>
     );
   }
