d151b481ff5fc8983b2e57f55bf25b030d8fa057
Ary Davani <arydavani@gmail.com>
2017-10-24 17:11:19 -0400

    homepage set up

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,16 +3,19 @@ import Info from './Info';
 import { NavLink } from 'react-router-dom';
 import Footer from './Footer';
 import PROJECT_URI from '../utility';
-import LandingPageNavbar from './LandingPageNavbar';
 
 export default class LandingPage extends Component {
   render(){
     return(
       <div>
-        <LandingPageNavbar />
+        {/* <LandingPageNavbar /> */}
         <div className="background3">
+          <div className="landing-nav">
+            <NavLink to={ PROJECT_URI + '/login' }>Login</NavLink>
+          </div>
           <div className="banner">
-            <i className="header">Calculate your menu items.</i>
+            <p className="title">Bite-Size</p>
+            <p className="header">Calculate your menu items.</p>
             <h4>Data-Bites makes it easy for restuarants to calculate calorie information for their menu items.</h4>
             <button onClick={() => { this.props.history.push(PROJECT_URI + '/signup') }} className="btn btn-success btn-lg">Sign Up Now!</button>
             <i className="fa fa-chevron-down fa-2x"></i>
