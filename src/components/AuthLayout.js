b4c5e8da5248333c1e4aa0da3ee3715d5ec9bc9f
Ary Davani <arydavani@gmail.com>
2017-10-29 11:55:27 -0400

    home button to login/signup screen:

--- src/components/AuthLayout.js
+++ src/components/AuthLayout.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import {NavLink} from 'react-router-dom';
+import PROJECT_URI from '../utility';
 
 export default class AuthLayout extends Component {
   render(){
     return (
       <div className="background2">
+        <div className="nav-options">
+          <NavLink to={ PROJECT_URI + '/' }><i className="fa fa-home fa-2x"></i></NavLink>
+        </div>
         {this.props.children}
       </div>
     )
